8274669e05c7ef14de7685e21cbc533632bde491
amidabrian51 <briankaty1@me.com>
2016-11-20 21:08:55 +0000

    final commit

--- instagram_clone/app/controllers/posts_controller.rb
+++ instagram_clone/app/controllers/posts_controller.rb
@@ -4,8 +4,26 @@ class PostsController < ApplicationController
   end
 
   def index
+    @post = Post.all
   end
 
   def show
+    @posts = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(permit_post)
+    if @post.save
+      flash[:success]= "Success"
+      redirect_to post_path(@post)
+    else
+      flash[:error] = @post.errors.full_message
+      rendirect_ro new_post_path
+    end
+  end
+
+  private
+  def permit_post
+    params.permit(:post).permit(:image,:description)
   end
 end
